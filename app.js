382930b8c0a01145baa72d4e6a7b78f78c5d8100
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2021-12-17 02:14:24 +0000

    adds menu toggle effect with transition

---
+++ app.js
@@ -0,0 +1,27 @@
+const toggleBtn = document.querySelector(".burger");
+const menuContainer = document.querySelector(".menu-container");
+const menu = document.querySelector(".menu");
+
+// helper function to setHeight of elements to enable toggle on form and menu
+function setHeight(element, height) {
+  element.style.height = `${height}px`;
+}
+
+function toggleElement(container, element) {
+  console.log(element.style.height);
+  let containerHeight = container.getBoundingClientRect().height;
+  let elementHeight = element.getBoundingClientRect().height;
+  console.log(containerHeight);
+  console.log(elementHeight);
+  if (containerHeight === 0) {
+    setHeight(container, elementHeight);
+  } else {
+    setHeight(container, 0);
+  }
+}
+
+// event listener on the burger (or toggleBtn) to toggle upon click by adjusting back to normal height if in minimised state
+// This allows toggle on the **Menu**
+toggleBtn.addEventListener("click", function () {
+  toggleElement(menuContainer, menu);
+});
