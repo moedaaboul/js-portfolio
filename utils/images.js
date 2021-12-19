a1b8514e03b6f7d3904ac25aecb0d7f6cf6eeb78
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2021-12-19 22:45:44 +0000

    updates url path in images.js to match names in github

--- utils/images.js
+++ utils/images.js
@@ -1,9 +1,9 @@
 export const images = [
   { name: "jobs-finder", path: "jobs-finder.gif" },
-  { name: "quiz-app", path: "quiz-app.gif" },
-  { name: "password-generator", path: "password-generator.png" },
+  { name: "quiz-app", path: "Quiz-App.gif" },
+  { name: "password-generator", path: "Password-Generator.png" },
   { name: "weather-dashboard", path: "weather-dashboard.png" },
-  { name: "work-day-scheduler", path: "work-day-scheduler.gif" },
+  { name: "work-day-scheduler", path: "Work-Day-Scheduler.gif" },
   { name: "flexbox-portfolio", path: "flexbox-portfolio.gif" },
 ];
 
