06756d4ebc20d47ef4f3c59d692b010cff553f27
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-03-08 22:15:59 +0000

    adds legogram to portfolio show

--- utils/getRepoData.js
+++ utils/getRepoData.js
@@ -1,24 +1,27 @@
-import { fetchGithubRepo } from "./fetchGithubRepo.js";
-import { renderRepoData } from "./renderRepoData.js";
-import { renderTags } from "./renderTags.js";
-import { fetchCommit } from "./fetchCommit.js";
+import { fetchGithubRepo } from './fetchGithubRepo.js';
+import { renderRepoData } from './renderRepoData.js';
+import { renderTags } from './renderTags.js';
+import { fetchCommit } from './fetchCommit.js';
 
-const repoModal = document.querySelector(".repo-modal");
-const repoList = document.querySelector(".portfolio");
-const url = "https://api.github.com/users/moedaaboul/repos";
+const repoModal = document.querySelector('.repo-modal');
+const repoList = document.querySelector('.portfolio');
+const url = 'https://api.github.com/users/moedaaboul/repos';
 
 // This adds an event listener on h3 headers to fetch repo data
 
-repoList.addEventListener("click", async function (event) {
+repoList.addEventListener('click', async function (event) {
   try {
-    if (event.target.tagName === "H3") {
+    if (event.target.tagName === 'H3') {
       const name = event.target.innerHTML;
       const data = await fetchGithubRepo(url);
       const contributors = await fetchCommit(name);
+      console.log(contributors);
       const contributions = contributors
-        .filter((e) => e.login === "moedaaboul")
+        .filter((e) => e.login === 'moedaaboul')
         .map((e) => e.contributions)[0];
+      console.log(contributions);
       const repoRaw = data.filter((obj) => obj.name === name)[0];
+      console.log(repoRaw);
       const repo = {
         title: repoRaw.name,
         url: repoRaw.html_url,
@@ -29,12 +32,13 @@ repoList.addEventListener("click", async function (event) {
         date: repoRaw.created_at,
         commits: contributions,
       };
+      console.log(repo);
       renderRepoData(repo); // after repos data is retrieved from fetch, individual repo info is rendered on screen through a modal
-      repoModal.classList.add("is-active"); // modal status to active to enable the following event listener to exit modal screen on modal-background click
+      repoModal.classList.add('is-active'); // modal status to active to enable the following event listener to exit modal screen on modal-background click
 
-      const modalBg = document.querySelector(".modal-background");
-      modalBg.addEventListener("click", function () {
-        repoModal.classList.remove("is-active");
+      const modalBg = document.querySelector('.modal-background');
+      modalBg.addEventListener('click', function () {
+        repoModal.classList.remove('is-active');
       });
     }
   } catch (error) {
--- utils/images.js
+++ utils/images.js
@@ -1,10 +1,12 @@
 export const images = [
-  { name: "jobs-finder", path: "jobs-finder.gif" },
-  { name: "quiz-app", path: "Quiz-App.gif" },
-  { name: "password-generator", path: "Password-Generator.png" },
-  { name: "weather-dashboard", path: "weather-dashboard.png" },
-  { name: "work-day-scheduler", path: "Work-Day-Scheduler.gif" },
-  { name: "flexbox-portfolio", path: "flexbox-portfolio.gif" },
+  { name: 'tech-blog', path: 'tech-blog.png' },
+  { name: 'legogram', path: 'legogram.png' },
+  { name: 'jobs-finder', path: 'jobs-finder.gif' },
+  { name: 'quiz-app', path: 'Quiz-App.gif' },
+  { name: 'password-generator', path: 'Password-Generator.png' },
+  { name: 'weather-dashboard', path: 'weather-dashboard.png' },
+  { name: 'work-day-scheduler', path: 'Work-Day-Scheduler.gif' },
+  { name: 'flexbox-portfolio', path: 'flexbox-portfolio.gif' },
 ];
 
 export const renderImages = (title) => {
