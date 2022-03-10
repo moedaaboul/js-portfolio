989fc05cc1a9c740e565c119247d086b03163fea
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-03-10 23:51:18 +0000

    modularise slider js logic

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import './utils/renderRepoData.js';
 import './utils/renderTags.js';
 import './utils/animation.js';
 import './utils/squareAnimation.js';
+import './utils/slider.js';
 
 const toggleBtn = document.querySelector('.burger');
 const menuContainer = document.querySelector('.menu-container');
@@ -32,21 +33,3 @@ function toggleElement(container, element) {
 toggleBtn.addEventListener('click', function () {
   toggleElement(menuContainer, menu);
 });
-
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName('mySlides');
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-
-  slides[slideIndex - 1].style.display = 'block';
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
