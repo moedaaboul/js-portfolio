7d528b6e1ea7ff44dbc60f946dea3d364244982f
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-03-11 22:15:47 +0000

    adds image snippets for portfolio

--- utils/images.js
+++ utils/images.js
@@ -8,7 +8,7 @@ export const images = [
   { name: 'work-day-scheduler', path: 'Work-Day-Scheduler.gif' },
   { name: 'flexbox-portfolio', path: 'flexbox-portfolio.gif' },
   { name: 'employee-tracker', path: 'flexbox-portfolio.gif' },
-  { name: 'team-profile-generator', path: 'flexbox-portfolio.gif' },
+  { name: 'team-profile-generator', path: 'team-profile-generator.png' },
 ];
 
 export const renderImages = (title) => {
